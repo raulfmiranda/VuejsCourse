d3bacd796dead72bff74083cc4ad649b45fbebe6
RaulMirandaOSF <raul.miranda@osf-commerce.com>
2019-07-08 11:56:59 -0300

    Done until section 13.176

--- sec11_148-forms/src/App.vue
+++ sec11_148-forms/src/App.vue
@@ -11,7 +11,9 @@
                                 type="text"
                                 id="email"
                                 class="form-control"
-                                v-model="userData.email">
+                                :value="userData.email"
+                                @input="userData.email = $event.target.value">
+                                <!-- v-model="userData.email"> -->
                     </div>
                     <div class="form-group">
                         <label for="password">Password</label>
@@ -51,13 +53,15 @@
                             <input
                                     type="checkbox"
                                     id="sendmail"
-                                    value="SendMail"> Send Mail
+                                    value="SendMail"
+                                    v-model="sendMail"> Send Mail
                         </label>
                         <label for="sendInfomail">
                             <input
                                     type="checkbox"
                                     id="sendInfomail"
-                                    value="SendInfoMail"> Send Infomail
+                                    value="SendInfoMail"
+                                    v-model="sendMail"> Send Infomail
                         </label>
                     </div>
 
@@ -69,13 +73,15 @@
                         <input
                                 type="radio"
                                 id="male"
-                                value="Male"> Male
+                                value="Male"
+                                v-model="gender"> Male
                     </label>
                     <label for="female">
                         <input
                                 type="radio"
                                 id="female"
-                                value="Female"> Female
+                                value="Female"
+                                v-model="gender"> Female
                     </label>
                 </div>
             </div>
@@ -84,22 +90,30 @@
                     <label for="priority">Priority</label>
                     <select
                             id="priority"
-                            class="form-control">
-                        <option></option>
+                            class="form-control"
+                            v-model="selectedPriority">
+                        <option 
+                            v-for="priority in priorities">{{ priority }}</option>
                     </select>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <app-switch v-model="dataSwitch"></app-switch>
                 </div>
             </div>
             <hr>
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <button
-                            class="btn btn-primary">Submit!
+                            class="btn btn-primary"
+                            @click.prevent="submitted">Submit!
                     </button>
                 </div>
             </div>
         </form>
         <hr>
-        <div class="row">
+        <div class="row" v-if="isSubmitted">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
@@ -112,11 +126,11 @@
                         <p style="white-space: pre">Message: {{ message }}</p>
                         <p><strong>Send Mail?</strong></p>
                         <ul>
-                            <li></li>
+                            <li v-for="item in sendMail">{{ item }}</li>
                         </ul>
-                        <p>Gender:</p>
-                        <p>Priority:</p>
-                        <p>Switched:</p>
+                        <p>Gender: {{ gender }}</p>
+                        <p>Priority: {{ selectedPriority }}</p>
+                        <p>Switched: {{ dataSwitch }}</p>
                     </div>
                 </div>
             </div>
@@ -125,6 +139,8 @@
 </template>
 
 <script>
+    import Switch from './Switch.vue';
+
     export default {
         data () {
             return {
@@ -133,8 +149,22 @@
                     password: '',
                     age: 27
                 },
-                message: 'A new Text'
+                message: 'A new Text',
+                sendMail: [],
+                gender: 'Male',
+                selectedPriority: 'High',
+                priorities: ['High', 'Medium', 'Low'],
+                dataSwitch: true,
+                isSubmitted: false
             }
+        },
+        methods: {
+            submitted() {
+                this.isSubmitted = true;
+            }
+        },
+        components: {
+            appSwitch: Switch
         }
     }
 </script>
